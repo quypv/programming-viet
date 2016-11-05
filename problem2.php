30b4042ec67c0a4ce714265f2eb6f02b8eaf59ac
viethd9 <proviethd@gmail.com>
2016-11-05 22:17:41 +0700

    done problem2!

--- problem2.php
+++ problem2.php
@@ -1,3 +1,16 @@
 <?php
-	$array = array("cam", "quýt","mít", "dừa");
-	print_r($array);
+	$fruit = array("cam", "quýt","mít", "dừa");
+	print_r($fruit);
+	echo "Độ dài của mảng: ".count($fruit)."</br>";
+	$fruitCount = array("cam"=>10,"quýt"=>22,"mít"=>4,"dừa"=>9);
+	print_r($fruitCount);
+	echo "Độ dài của mảng: ".count($fruitCount)."</br>";
+	echo $fruitCount['quýt']."</br>";
+	$students = array (
+    'A1' => ['Nam', 'Hải', 'Tuấn'],
+    'A2' => ['Hiền', 'Dung', 'Tâm'],
+    'A3' => ['Phong', 'Ly', 'Cường']
+	);
+	echo $students['A1'][0]."</br>";
+	echo $students['A2'][2]."</br>";
+	echo $students['A3'][1];
